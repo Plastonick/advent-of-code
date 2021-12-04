4563a517cb901d16e4326cc7f353bf0ba5a749d0
David Pugh <dpugh@podfather.com>
2021-12-04 23:05:01 +0000

    day 4 part 1

---
+++ 2021/04 - Giant Squid/main.py
@@ -0,0 +1,50 @@
+import numpy as np
+from typing import List
+
+numbers = [int(n) for n in np.genfromtxt("./numbers", delimiter=",")]
+
+
+def boards_from_file(f):
+    ret = []
+    curr = []
+    for line in f:
+        if len(line) < 2:
+            if len(curr) > 0:
+                ret.append(curr)
+            curr = []
+        else:
+            curr.append([int(n) for n in line.split(" ")])
+
+    return ret
+
+
+def a_row_is_complete(b: List[List[int]]) -> bool:
+    for r in b:
+        if sum(r) == 0:
+            return True
+
+    return False
+
+
+def is_complete(b):
+    return a_row_is_complete(b) or a_row_is_complete(list(map(list, zip(*b))))
+
+
+def board_score(b, n):
+    total = 0
+    for r in b:
+        total += sum(r)
+    return total * n
+
+
+with open("./input") as file:
+    boards = boards_from_file(file)
+
+for num in numbers:
+    for board in boards:
+        for i in range(len(board)):
+            board[i] = [0 if n == num else n for n in board[i]]
+
+        if is_complete(board):
+            print(board_score(board, num))
+            exit()
