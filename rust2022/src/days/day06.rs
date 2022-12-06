1a7ac559e53f103171084704860c28ad0abb7082
David Pugh <plastonick@gmail.com>
2022-12-06 07:56:25 +0000

    day 6 part 2

--- rust2022/src/days/day06.rs
+++ rust2022/src/days/day06.rs
@@ -7,26 +7,40 @@ pub fn run() {
 
     let chars = buffer.chars();
     let mut last_four: Vec<char> = Vec::new();
+    let mut last_fourteen: Vec<char> = Vec::new();
     let mut counter = 0;
+    let mut start_of_packet = 0;
+    let mut start_of_message = 0;
 
     for character in chars {
         last_four.push(character);
+        last_fourteen.push(character);
         counter += 1;
 
         if last_four.len() > 4 {
             last_four.remove(0);
-        } else if last_four.len() < 4 {
-            continue;
         }
 
-        if are_unique(&last_four) {
-            break;
+        if last_fourteen.len() > 14 {
+            last_fourteen.remove(0);
+        }
+
+        if last_four.len() == 4 && are_unique(&last_four) && start_of_packet == 0 {
+            start_of_packet = counter;
+        }
+
+        if last_fourteen.len() == 14 && are_unique(&last_fourteen) && start_of_message == 0 {
+            start_of_message = counter;
         }
     }
 
     println!(
-        "Day 06, Part 1: The first all unique group of characters occurs at position {}",
-        counter
+        "Day 06, Part 1: The start of packet marker occurs at position {}",
+        start_of_packet
+    );
+    println!(
+        "Day 06, Part 2: The start of message marker occurs at position {}",
+        start_of_message
     );
 }
 
