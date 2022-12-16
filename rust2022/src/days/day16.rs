f75773dbfc3cd18903602b79227a44671c75ff8d
David Pugh <plastonick@gmail.com>
2022-12-16 18:54:31 +0000

    further WIP

--- rust2022/src/days/day16.rs
+++ rust2022/src/days/day16.rs
@@ -1,7 +1,4 @@
-use std::{
-    cmp::max,
-    collections::{HashMap, HashSet},
-};
+use std::{cmp::max, collections::HashMap};
 
 use regex::Regex;
 
@@ -43,26 +40,11 @@ impl Valve {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
 struct State {
     position: String,
     ttl: isize,
-    open: HashSet<String>,
-    released: isize,
-}
-
-impl State {
-    fn cache_key(&self) -> String {
-        let mut key = format!("{}-", self.position);
-
-        for valve in &self.open {
-            key += &format!(".{}", valve);
-        }
-
-        key += &format!("_{}", self.ttl);
-
-        String::from(key)
-    }
+    open: Vec<String>,
 }
 
 pub fn run(_: bool) {
@@ -76,21 +58,42 @@ pub fn run(_: bool) {
         .collect();
     let time_to_eruption = 30;
 
-    // let mut states = vec![State {
-    //     position: String::from("AA"),
-    //     ttl: time_to_eruption,
-    //     open: HashSet::new(),
-    //     released: 0,
-    // }];
+    // let mut cache = HashMap::new();
+    // let best_value = get_best_value(
+    //     (
+    //         State {
+    //             position: String::from("AA"),
+    //             ttl: time_to_eruption,
+    //             open: Vec::new(),
+    //         },
+    //         0,
+    //     ),
+    //     &valves,
+    //     &mut cache,
+    // );
 
-    // let mut end_states = Vec::new();
+    let start = State {
+        position: String::from("AA"),
+        ttl: time_to_eruption,
+        open: Vec::new(),
+    };
+    // let mut states = Vec::new();
+    // states.push(start);
+
+    // let mut best = 0;
+    let mut cache = HashMap::new();
 
     // loop {
-    //     for state in states.pop() {
-    //         if state.ttl <= 0 {
-    //             // the volcano's already erupted, I hope I've done enough!
+    //     for (state, released) in states.pop() {
+    //         let time_expired = state.ttl <= 0;
+    //         let all_valves_open = state.open.len() == valves.len();
+    //         let best_possible = 50; // TODO
 
-    //             end_states.push(state);
+    //         if time_expired || all_valves_open || best_possible < best {
+    //             best = released;
+
+    //             cache.insert(state, released);
+
     //             continue;
     //         }
 
@@ -99,87 +102,86 @@ pub fn run(_: bool) {
     //         // can we open the current valve?
     //         if !state.open.contains(&state.position) {
     //             let mut now_open = state.open.clone(); // TODO clone is bad mkay
-    //             now_open.insert(at_valve.name.clone()); // TODO clone is bad mkay
+    //             now_open.push(at_valve.name.clone()); // TODO clone is bad mkay
+    //             now_open.sort();
 
     //             // try opening the current state
-    //             states.push(State {
-    //                 released: state.released + (at_valve.rate * state.ttl),
+    //             let try_state = State {
     //                 open: now_open,
     //                 ttl: state.ttl - 1,
-    //                 position: state.position,
-    //             })
+    //                 position: state.position.clone(),
+    //             };
+
+    //             states.push((try_state, released + (at_valve.rate * state.ttl)));
     //         }
 
     //         // can we go to a different valve?
     //         for valve_name in &at_valve.leads_to {
-    //             states.push(State {
+    //             let try_state = State {
     //                 position: valve_name.clone(),
     //                 ttl: state.ttl - 1,
     //                 open: state.open.clone(), // TODO clone is bad mkay
-    //                 released: state.released,
-    //             })
+    //             };
+
+    //             states.push((try_state, released));
     //         }
     //     }
 
+    //     // when we've not got any more states to look over
     //     if states.len() == 0 {
     //         break;
     //     }
     // }
 
-    let mut cache = HashMap::new();
-    let best_value = get_best_value(
-        State {
-            position: String::from("AA"),
-            ttl: time_to_eruption,
-            open: HashSet::new(),
-            released: 0,
-        },
-        &valves,
-        &mut cache,
-    );
+    let best = get_future_value(start, &valves, &mut cache);
+
+    println!("Cache size: {}", cache.len());
 
     println!(
         "Day 16, Part 1: The most I can release in {} minutes is {}",
-        time_to_eruption, best_value
+        time_to_eruption, best
     );
 }
 
-fn get_best_value(
+fn get_future_value(
     state: State,
     valves: &HashMap<String, Valve>,
-    best_cache: &mut HashMap<String, isize>,
+    value_cache: &mut HashMap<State, isize>,
 ) -> isize {
-    let key = state.cache_key();
-
-    if let Some(cache_value) = best_cache.get(&key) {
+    // if we've already been in this state before, return how valuable it was
+    if let Some(cache_value) = value_cache.get(&state) {
         return cache_value.to_owned();
     }
 
-    if state.ttl <= 0 {
-        // the volcano's already erupted, I hope I've done enough!
+    let time_expired = state.ttl <= 0;
+    let all_valves_open = state.open.len() == valves.len();
 
-        best_cache.insert(key, state.released);
+    if time_expired || all_valves_open {
+        // the volcano's erupted, we can't improve, I hope I've done enough!
 
-        return state.released;
+        return 0;
     }
 
+    // enumerate the possible actions and see what the most valuable action is
     let at_valve = valves.get(&state.position).unwrap();
-    let mut best_value = 0;
+    let mut best_increase = 0;
 
     // can we open the current valve?
     if !state.open.contains(&state.position) {
         let mut now_open = state.open.clone(); // TODO clone is bad mkay
-        now_open.insert(at_valve.name.clone()); // TODO clone is bad mkay
+        now_open.push(at_valve.name.clone()); // TODO clone is bad mkay
+        now_open.sort();
 
         // try opening the current state
         let try_state = State {
-            released: state.released + (at_valve.rate * state.ttl),
             open: now_open,
             ttl: state.ttl - 1,
-            position: state.position,
+            position: state.position.clone(),
         };
 
-        best_value = max(best_value, get_best_value(try_state, valves, best_cache));
+        let future_value = get_future_value(try_state, valves, value_cache);
+        let valve_open_value = at_valve.rate * state.ttl;
+        best_increase = max(best_increase, valve_open_value + future_value);
     }
 
     // can we go to a different valve?
@@ -188,13 +190,13 @@ fn get_best_value(
             position: valve_name.clone(),
             ttl: state.ttl - 1,
             open: state.open.clone(), // TODO clone is bad mkay
-            released: state.released,
         };
 
-        best_value = max(best_value, get_best_value(try_state, valves, best_cache));
+        let future_value = get_future_value(try_state, valves, value_cache);
+        best_increase = max(best_increase, future_value);
     }
 
-    best_cache.insert(key, best_value);
+    value_cache.insert(state, best_increase);
 
-    best_value
+    best_increase
 }
