831c9c320000340ab05b6ff3ec1396ca7d8cb3df
David Pugh <plastonick@gmail.com>
2022-12-17 21:03:33 +0000

    day 17 part 2, surely this is basically correct...

--- rust2022/src/days/day17.rs
+++ rust2022/src/days/day17.rs
@@ -1,4 +1,7 @@
-use std::cmp::max;
+use std::{
+    cmp::max,
+    collections::{HashMap, HashSet},
+};
 
 use crate::common::get_file_contents;
 
@@ -8,39 +11,40 @@ enum Direction {
     Right,
 }
 
-pub fn run(visual: bool) {
-    let file = get_file_contents("day17");
+pub fn run(_: bool) {
+    let file = get_file_contents("day17-test");
     let directions = file
         .as_bytes()
         .iter()
-        .map(|x| {
-            if *x as char == '<' {
-                Direction::Left
-            } else {
-                Direction::Right
+        .map(|x| match *x as char {
+            '<' => Direction::Left,
+            '>' => Direction::Right,
+            _ => {
+                println!("Uh oh! This isn't a direction...");
+                panic!()
             }
         })
         .collect::<Vec<_>>();
 
     // Blocks
-    //
-    // ####
-    //
-    // .#.
-    // ###
-    // .#.
-    //
-    // ..#
-    // ..#
-    // ###
-    //
-    // #
-    // #
-    // #
-    // #
-    //
-    // ##
-    // ##
+
+    // | #### |
+
+    // |  #  |
+    // | ### |
+    // |  #  |
+
+    // |   # |
+    // |   # |
+    // | ### |
+
+    // | # |
+    // | # |
+    // | # |
+    // | # |
+
+    // | ## |
+    // | ## |
 
     let blocks = [
         vec![(0, 0), (1, 0), (2, 0), (3, 0)],
@@ -50,80 +54,137 @@ pub fn run(visual: bool) {
         vec![(0, 0), (1, 0), (0, 1), (1, 1)],
     ];
 
-    let mut max_height = 0;
+    let limits: [usize; 2] = [2022, 1_000_000_000_000];
+    for (i, limit) in limits.iter().enumerate() {
+        let height = height_after_blocks(limit.to_owned(), &blocks, &directions);
 
-    let mut block_index = 0;
+        println!(
+            "Day 17, Part {}: The tetris board is {} units high after {} moves",
+            i + 1,
+            height,
+            limit
+        );
+    }
+}
+
+fn height_after_blocks(
+    limit: usize,
+    blocks: &[Vec<(isize, isize)>; 5],
+    directions: &Vec<Direction>,
+) -> isize {
+    let mut block_num = 0;
     let mut direction_index = 0;
-    let mut board: Vec<(isize, isize)> = Vec::new();
-    // let total_moves = 1_000_000_000_000;
-    let total_moves = 3000;
-    let part_1_limit = 2022;
-    let mut part_1_solution = 0;
+    let mut board: HashSet<(isize, isize)> = HashSet::new();
+    let mut max_height = 0;
+    let mut cache: HashMap<(usize, usize), (isize, usize)> = HashMap::new();
+    let mut cycle_detected = false;
 
-    while block_index < total_moves {
-        if block_index == 2022 {
-            part_1_solution = max_height;
-        }
+    while block_num < limit {
+        let key = (block_num % blocks.len(), direction_index);
 
-        let block = &blocks[block_index % blocks.len()];
+        // go until we've detected a loop
+        if !cycle_detected {
+            if let Some((height, prev_block_num)) = cache.get(&key) {
+                cycle_detected = true;
+                let cycle_height = max_height - height.to_owned();
+                let cycle_length = block_num - prev_block_num;
+                let old_max_height = max_height;
 
-        let mut height = max_height + 3;
-        let mut left = 2;
+                // increase the loop a bunch!
+                let cycles_remaining: usize = ((limit - block_num) / cycle_length) - 1;
+                max_height += cycles_remaining as isize * cycle_height;
+                block_num += cycles_remaining * cycle_length;
 
-        loop {
-            // apply wind
-            let direction = match &directions[direction_index] {
-                Direction::Left => -1,
-                Direction::Right => 1,
-            };
+                // copy the last cycle
+                for y in 0..cycle_height {
+                    for x in 0..7 {
+                        let old_key = (x, old_max_height - y);
+                        let new_key = (x, max_height - y);
 
-            direction_index = (direction_index + 1) % directions.len();
-
-            // move the block with the wind!
-            left += direction;
-
-            // if the block can't exist with this new left_offset, revert the offset
-            if !can_exist(height, left, block, &board) {
-                left -= direction;
-            }
-
-            // move block down
-            height -= 1;
-
-            // if the block can't exist lower down, revert the height drop, freeze the block there, and terminate
-            if !can_exist(height, left, block, &board) {
-                height += 1;
-                // add the block to the board, and re-calculate the max height
-                for point in block {
-                    let x = point.0 + left;
-                    let y = point.1 + height;
-
-                    max_height = max(max_height, y + 1);
-                    board.push((x, y));
+                        if board.contains(&old_key) {
+                            board.insert(new_key);
+                        }
+                    }
                 }
-
-                break;
             }
         }
 
-        block_index += 1;
+        (direction_index, max_height) = iterate(
+            block_num,
+            direction_index,
+            max_height,
+            &blocks,
+            &directions,
+            &mut board,
+        );
+
+        cache.insert(key, (max_height, block_num));
+
+        block_num += 1;
     }
 
-    if visual {
-        print(&board);
+    max_height
+}
+
+fn iterate(
+    block_idx: usize,
+    direction_index: usize,
+    max_height: isize,
+    blocks: &[Vec<(isize, isize)>; 5],
+    directions: &Vec<Direction>,
+    board: &mut HashSet<(isize, isize)>,
+) -> (usize, isize) {
+    let block = &blocks[block_idx % blocks.len()];
+
+    let mut out_max_height = max_height;
+    let mut out_direction_index = direction_index;
+    let mut height = max_height + 3;
+    let mut left = 2;
+
+    loop {
+        // apply wind
+        let direction = match &directions[out_direction_index] {
+            Direction::Left => -1,
+            Direction::Right => 1,
+        };
+
+        out_direction_index = (out_direction_index + 1) % directions.len();
+
+        // move the block with the wind!
+        left += direction;
+
+        // if the block can't exist with this new left_offset, revert the offset
+        if !can_exist(height, left, block, &board) {
+            left -= direction;
+        }
+
+        // move block down
+        height -= 1;
+
+        // if the block can't exist lower down, revert the height drop, freeze the block there, and terminate
+        if !can_exist(height, left, block, &board) {
+            height += 1;
+            // add the block to the board, and re-calculate the max height
+            for point in block {
+                let x = point.0 + left;
+                let y = point.1 + height;
+
+                out_max_height = max(out_max_height, y + 1);
+                board.insert((x, y));
+            }
+
+            break;
+        }
     }
 
-    println!(
-        "Day 17, Part 1: The tetris board is {} units high after {} moves",
-        part_1_solution, part_1_limit
-    );
+    (out_direction_index, out_max_height)
 }
 
 fn can_exist(
     height: isize,
     left: isize,
     block: &Vec<(isize, isize)>,
-    board: &Vec<(isize, isize)>,
+    board: &HashSet<(isize, isize)>,
 ) -> bool {
     if height < 0 || left < 0 || left > 6 {
         return false;
@@ -145,7 +206,7 @@ fn can_exist(
     true
 }
 
-fn print(board: &Vec<(isize, isize)>) {
+fn print(board: &HashSet<(isize, isize)>) {
     let mut height = 0;
     let width = 7;
 
