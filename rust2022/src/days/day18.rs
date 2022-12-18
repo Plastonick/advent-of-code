1ced3fd92d3c93aa95057b60ad08b3bd2d37acc7
David Pugh <plastonick@gmail.com>
2022-12-18 13:27:53 +0000

    improve day 18 performance

--- rust2022/src/days/day18.rs
+++ rust2022/src/days/day18.rs
@@ -1,4 +1,7 @@
-use std::collections::HashSet;
+use std::{
+    cmp::{max, min},
+    collections::HashSet,
+};
 
 use crate::common::get_lines;
 
@@ -41,6 +44,17 @@ fn internal_area_of_points(points: &HashSet<(isize, isize, isize)>) -> usize {
     let mut total_surface_area = surface_area_of_points(&points);
     let mut volume_points = HashSet::new();
     let mut outside = HashSet::new();
+    let mut limits = [[isize::MAX, isize::MIN]; 3];
+
+    for (x, y, z) in points {
+        let [x_limits, y_limits, z_limits] = limits;
+
+        limits = [
+            [min(x_limits[0], *x), max(x_limits[1], *x)],
+            [min(y_limits[0], *y), max(y_limits[1], *y)],
+            [min(z_limits[0], *z), max(z_limits[1], *z)],
+        ];
+    }
 
     for point in points {
         for n in get_neighbours(point) {
@@ -48,7 +62,7 @@ fn internal_area_of_points(points: &HashSet<(isize, isize, isize)>) -> usize {
                 continue;
             }
 
-            if let Some(volume) = map_volume(n, points, &mut outside) {
+            if let Some(volume) = map_volume(n, points, &mut outside, &limits) {
                 total_surface_area -= surface_area_of_points(&volume);
 
                 volume_points = volume_points.union(&volume).copied().collect();
@@ -64,6 +78,7 @@ fn map_volume(
     start: (isize, isize, isize),
     boundary: &HashSet<(isize, isize, isize)>,
     outside: &mut HashSet<(isize, isize, isize)>,
+    limits: &[[isize; 2]; 3],
 ) -> Option<HashSet<(isize, isize, isize)>> {
     // trivially return None if we're starting at a boundary point
     if boundary.contains(&start) {
@@ -79,8 +94,8 @@ fn map_volume(
             // get neighbours
             let neighbours = get_neighbours(point);
             for n in neighbours {
-                // we must have started from outside, update the outside points and return None
-                if outside.contains(&n) || n == (0, 0, 0) {
+                // we must have started from outside, update the known outside points and return None
+                if is_outside(&n, limits, outside) {
                     for p in volume {
                         outside.insert(p);
                     }
@@ -108,6 +123,42 @@ fn map_volume(
     }
 
     Some(volume)
+}
+
+fn is_outside(
+    point: &(isize, isize, isize),
+    limits: &[[isize; 2]; 3],
+    outside: &HashSet<(isize, isize, isize)>,
+) -> bool {
+    if outside.contains(&point) {
+        return true;
+    }
+
+    if point.0 < limits[0][0] {
+        return true;
+    }
+
+    if point.0 > limits[0][1] {
+        return true;
+    }
+
+    if point.1 < limits[1][0] {
+        return true;
+    }
+
+    if point.1 > limits[1][1] {
+        return true;
+    }
+
+    if point.2 < limits[2][0] {
+        return true;
+    }
+
+    if point.2 > limits[2][1] {
+        return true;
+    }
+
+    return false;
 }
 
 fn surface_area_of_points(points: &HashSet<(isize, isize, isize)>) -> usize {
