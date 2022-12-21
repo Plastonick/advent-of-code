a9425f61335c1e25e5154eabfe6c16d7a2a68c27
David Pugh <plastonick@gmail.com>
2022-12-21 23:49:21 +0000

    nearly working branch + bound solution ontop of dfs

--- rust2022/src/days/day16.rs
+++ rust2022/src/days/day16.rs
@@ -50,6 +50,16 @@ struct State {
     players_remaining: u8,
     ttl: usize,
     open: usize,
+    initial_ttl: usize,
+    initial_pos: usize,
+}
+
+impl State {
+    fn is_valve_open(&self, valve: &Valve) -> bool {
+        let bit_mask = 2 << valve.index;
+
+        self.open & bit_mask != 0
+    }
 }
 
 pub fn run(args: &Args) -> (String, String) {
@@ -65,23 +75,16 @@ pub fn run(args: &Args) -> (String, String) {
         name_index_map.insert(name, index);
     }
 
-    let valves: HashMap<usize, Valve> = lines
+    let valves: Vec<Valve> = lines
         .iter()
-        .map(|x| {
-            let valve = Valve::from_str(x, &name_index_map);
-            (valve.index.to_owned(), valve)
-        })
+        .map(|x| Valve::from_str(x, &name_index_map))
         .collect();
 
     let matrix: HashMap<(usize, usize), usize> = build_distance_matrix(&valves);
     let mut cache = HashMap::new();
 
     // don't consider any valves with a 0 flow
-    let flow_valves = valves
-        .iter()
-        .filter(|(_, x)| x.rate > 0)
-        .map(|(i, x)| (i.to_owned(), x))
-        .collect::<HashMap<_, _>>();
+    let flow_valves = valves.iter().filter(|x| x.rate > 0).collect::<Vec<_>>();
 
     let start_pos = name_index_map.get("AA").unwrap().to_owned();
     let best_solo = get_future_value(
@@ -90,20 +93,27 @@ pub fn run(args: &Args) -> (String, String) {
             ttl: 30,
             players_remaining: 0,
             open: 0,
+            initial_ttl: 30,
+            initial_pos: start_pos,
         },
-        (start_pos, 30),
+        0,
+        &mut 0,
         &flow_valves,
         &matrix,
         &mut cache,
     );
+
     let best_with_elephant = get_future_value(
         State {
             position: start_pos,
             ttl: 26,
             players_remaining: 1,
             open: 0,
+            initial_ttl: 26,
+            initial_pos: start_pos,
         },
-        (start_pos, 26),
+        0,
+        &mut 0,
         &flow_valves,
         &matrix,
         &mut cache,
@@ -125,66 +135,87 @@ pub fn run(args: &Args) -> (String, String) {
 
 fn get_future_value(
     state: State,
-    initial: (usize, usize),
-    valves: &HashMap<usize, &Valve>,
+    curr_value: usize,
+    lower_bound: &mut usize,
+    valves: &Vec<&Valve>,
     distances: &HashMap<(usize, usize), usize>,
     value_cache: &mut HashMap<State, usize>,
 ) -> usize {
     // if we've already been in this state before, return how valuable it was last
     if let Some(cache_value) = value_cache.get(&state) {
-        return cache_value.to_owned();
+        *lower_bound = max(*lower_bound, curr_value + *cache_value);
+
+        return *cache_value;
     }
 
     // we need at least three ticks to move, open, and reap the reward from pressure release
     let time_expired = state.ttl < 3;
     if time_expired {
         // the volcano's erupted, we can't improve, I hope I've done enough!
+        *lower_bound = max(*lower_bound, curr_value);
+
+        return 0;
+    }
+
+    // what's a cheap upper bound for how we could improve this state?
+    let upper_bound = upper_bound(&state, valves, distances);
+    if upper_bound + curr_value < *lower_bound {
+        // this option can't improve on the best we've found
         return 0;
     }
 
     // enumerate the possible actions and see what the most valuable action is
     let mut best_increase = 0;
 
-    // go through all closed valves and try opening them!
-    for (_, try_valve) in valves {
+    // go through all the valves and try opening them!
+    for try_valve in valves {
         // have I been here?
-        let bit_mask = 2 << try_valve.index;
-        if state.open & bit_mask != 0 {
+        if state.is_valve_open(try_valve) {
             // we've already opened this one, no reason to open it again
             continue;
         }
 
         let distance = distances[&(state.position, try_valve.index)];
+        let time_to_open = distance + 1;
 
         // do we have time to move here and open the valve and let that valve run for at least 1 tick?
-        if state.ttl < distance + 1 {
+        if state.ttl <= time_to_open {
             // no, don't bother trying this one
             continue;
         }
 
-        let new_ttl = state.ttl - distance - 1;
+        let new_ttl = state.ttl - time_to_open;
         let try_state = State {
             position: try_valve.index,
             ttl: new_ttl,
-            open: state.open + bit_mask, // "close" the valve we're trying!
+            open: state.open + (2 << try_valve.index), // "close" the valve we're trying!
             ..state
         };
 
-        let future_value = get_future_value(try_state, initial, valves, distances, value_cache);
-        best_increase = max(best_increase, future_value + (new_ttl * try_valve.rate));
+        let valve_value = new_ttl * try_valve.rate;
+
+        let future_value = get_future_value(
+            try_state,
+            curr_value + valve_value,
+            lower_bound,
+            valves,
+            distances,
+            value_cache,
+        );
+        best_increase = max(best_increase, future_value + valve_value);
     }
 
-    // add a "do nothing" option where we immediately pass the buck to the other player
+    // add a "do nothing" option where we immediately pass the buck to the next player
     if state.players_remaining > 0 {
-        let (initial_pos, initial_ttl) = initial;
         let future_value = get_future_value(
             State {
-                position: initial_pos,
+                position: state.initial_pos,
+                ttl: state.initial_ttl,
                 players_remaining: state.players_remaining - 1,
-                ttl: initial_ttl,
                 ..state
             },
-            initial,
+            curr_value,
+            lower_bound,
             valves,
             distances,
             value_cache,
@@ -198,11 +229,11 @@ fn get_future_value(
     best_increase
 }
 
-fn build_distance_matrix(valves: &HashMap<usize, Valve>) -> HashMap<(usize, usize), usize> {
+fn build_distance_matrix(valves: &Vec<Valve>) -> HashMap<(usize, usize), usize> {
     let mut matrix: HashMap<(usize, usize), usize> = HashMap::new();
 
-    for (_, a) in valves {
-        for (_, b) in valves {
+    for a in valves {
+        for b in valves {
             // we only need to do one direction, then trivially duplicate it
             if a.index <= b.index {
                 continue;
@@ -218,7 +249,7 @@ fn build_distance_matrix(valves: &HashMap<usize, Valve>) -> HashMap<(usize, usiz
     matrix
 }
 
-fn shortest_distance_between(a: usize, b: usize, valves: &HashMap<usize, Valve>) -> usize {
+fn shortest_distance_between(a: usize, b: usize, valves: &Vec<Valve>) -> usize {
     let mut dist = 0;
     let mut wave: Vec<&usize> = vec![&a];
     let mut visited = HashSet::new();
@@ -235,7 +266,7 @@ fn shortest_distance_between(a: usize, b: usize, valves: &HashMap<usize, Valve>)
         let mut next_wave = Vec::new();
 
         for indx in wave {
-            let valve = valves.get(&indx).unwrap();
+            let valve = &valves[*indx];
 
             for neighbour in &valve.leads_to {
                 // if we've already visited this valve,
@@ -263,4 +294,39 @@ fn shortest_distance_between(a: usize, b: usize, valves: &HashMap<usize, Valve>)
 
     println!("We should always have a route between any two valves...");
     panic!()
+}
+
+fn upper_bound(
+    state: &State,
+    valves: &Vec<&Valve>,
+    distances: &HashMap<(usize, usize), usize>,
+) -> usize {
+    let mut upper_bound = 0;
+
+    for valve in valves {
+        if state.is_valve_open(valve) {
+            continue;
+        }
+
+        let (ttl, position) = if state.players_remaining > 0 {
+            // we don't really know when the valves could be opened,
+            // so assume they're opened ASAP from the start
+            (state.initial_ttl, state.initial_pos)
+        } else {
+            (state.ttl, state.position)
+        };
+
+        let distance = distances[&(position, valve.index)];
+        let time_to_open = distance + 1;
+
+        if ttl <= time_to_open {
+            continue;
+        }
+
+        let new_ttl = ttl - time_to_open;
+
+        upper_bound += new_ttl * valve.rate;
+    }
+
+    upper_bound
 }
