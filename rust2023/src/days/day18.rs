83df6732fecf89fd8ebbf96611e783bf6ab5c67a
David Pugh <plastonick@gmail.com>
2023-12-28 14:48:57 +0000

    day 18 clever area method day 19 parts 1 and 2

--- rust2023/src/days/day18.rs
+++ rust2023/src/days/day18.rs
@@ -1,7 +1,7 @@
 use crate::common::{get_lines, Answer};
-use crate::maps::{_print, find_inner_points, Vector};
+use crate::maps::Vector;
 use crate::Args;
-use std::collections::HashSet;
+use std::isize;
 
 pub fn run(_args: &Args) -> Answer {
     let lines = if _args.test {
@@ -10,11 +10,52 @@ pub fn run(_args: &Args) -> Answer {
         get_lines("day18")
     };
 
-    let instructions = lines
+    let instructions_part_1 = build_instructions_part1(&lines);
+    let instructions_part_2 = build_instructions_part2(&lines);
+
+    (
+        find_interior_volume(&instructions_part_1).to_string(),
+        find_interior_volume(&instructions_part_2).to_string(),
+    )
+}
+
+fn find_interior_volume(instructions: &Vec<(Vector, isize)>) -> isize {
+    // Finding the area of a polygon with known ordered vertices: https://www.topcoder.com/thrive/articles/Geometry%20Concepts%20part%201:%20Basic%20Concepts#PolygonArea
+    // slightly tweaking it, since we're adding a half-width "wrapper" around that shape.
+
+    let mut digger = Vector { row: 0, col: 0 };
+    let mut corners: Vec<Vector> = Vec::new();
+
+    let mut wrapper_area = 1;
+    for (direction, count) in instructions {
+        wrapper_area += count;
+
+        digger = digger.add(&direction.mul(*count));
+        corners.push(digger);
+    }
+
+    wrapper_area /= 2;
+    wrapper_area += 1;
+
+    let mut internal_area = 0;
+    for (i, corner) in corners.iter().enumerate() {
+        let j = (i + 1) % corners.len();
+        let next_corner = corners[j];
+
+        internal_area += (corner.row + 3) * (next_corner.col + 3);
+        internal_area -= (corner.col + 3) * (next_corner.row + 3);
+    }
+
+    internal_area = internal_area.abs() / 2;
+    internal_area + wrapper_area
+}
+
+fn build_instructions_part1(lines: &Vec<String>) -> Vec<(Vector, isize)> {
+    lines
         .iter()
         .map(|line| {
             let (dir, rest) = line.split_once(' ').unwrap();
-            let (count, colour) = rest.split_once(' ').unwrap();
+            let (count, _) = rest.split_once(' ').unwrap();
 
             let direction = match dir {
                 "R" => Vector { row: 0, col: 1 },
@@ -26,25 +67,30 @@ pub fn run(_args: &Args) -> Answer {
                 }
             };
 
-            (direction, count.parse::<isize>().unwrap(), colour)
+            (direction, count.parse::<isize>().unwrap())
         })
-        .collect::<Vec<_>>();
+        .collect::<Vec<_>>()
+}
 
-    let mut digger = Vector { row: 0, col: 0 };
-    let mut path = vec![digger];
-    for (direction, count, _) in instructions {
-        for _ in 0..count {
-            digger = digger.add(&direction);
+fn build_instructions_part2(lines: &Vec<String>) -> Vec<(Vector, isize)> {
+    lines
+        .iter()
+        .map(|line| {
+            let (_, hex) = line.split_once('#').unwrap();
 
-            path.push(digger);
-        }
-    }
+            let count = isize::from_str_radix(&hex[0..5], 16).unwrap();
+            let dir_char = hex.chars().nth(5).unwrap();
+            let direction = match dir_char {
+                '0' => Vector { row: 0, col: 1 },
+                '2' => Vector { row: 0, col: -1 },
+                '3' => Vector { row: -1, col: 0 },
+                '1' => Vector { row: 1, col: 0 },
+                _ => {
+                    panic!("Unexpected direction '{dir_char}'")
+                }
+            };
 
-    let path_points = path.clone().into_iter().collect::<HashSet<Vector>>();
-    let mut points = find_inner_points(&path);
-    points.extend(path_points);
-
-    _print(&points);
-
-    (points.len().to_string(), "".to_string())
+            (direction, count)
+        })
+        .collect::<Vec<_>>()
 }
--- rust2023/src/days/day19.rs
+++ rust2023/src/days/day19.rs
@@ -1,6 +1,200 @@
-use crate::common::Answer;
+use crate::common::{get_file_contents, Answer};
 use crate::Args;
+use std::collections::HashMap;
+
+#[derive(Debug)]
+struct Rule {
+    goto: String,
+    condition: Option<(char, char, isize)>,
+}
+
+type Rating = HashMap<char, isize>;
+type Workflows = HashMap<String, Vec<Rule>>;
 
 pub fn run(_args: &Args) -> Answer {
-    ("".to_string(), "".to_string())
+    let file = if _args.test {
+        get_file_contents("day19-test")
+    } else {
+        get_file_contents("day19")
+    };
+
+    let (workflow_lines, rating_lines) = file.split_once("\n\n").unwrap();
+
+    let workflows = workflow_lines
+        .lines()
+        .map(|line| {
+            let (name, ruleset) = line.split_once('{').unwrap();
+
+            let rules = ruleset
+                .trim_end_matches('}')
+                .split(',')
+                .map(|r| {
+                    if let Some((condition, goto)) = r.split_once(':') {
+                        let (lhs_operator, value) = condition.split_at(2);
+
+                        Rule {
+                            goto: goto.to_owned(),
+                            condition: Some((
+                                lhs_operator.chars().nth(0).unwrap(),
+                                lhs_operator.chars().nth(1).unwrap(),
+                                value.parse::<isize>().unwrap(),
+                            )),
+                        }
+                    } else {
+                        Rule {
+                            goto: r.to_owned(),
+                            condition: None,
+                        }
+                    }
+                })
+                .collect::<Vec<Rule>>();
+
+            (name.to_string(), rules)
+        })
+        .collect::<Workflows>();
+
+    let ratings = rating_lines
+        .lines()
+        .map(|line| {
+            line.trim_matches(['{', '}'].as_slice())
+                .split(',')
+                .map(|m| {
+                    let (category, value) = m.split_once('=').unwrap();
+
+                    (
+                        category.chars().next().unwrap(),
+                        value.parse::<isize>().unwrap(),
+                    )
+                })
+                .collect::<Rating>()
+        })
+        .collect::<Vec<_>>();
+
+    let part_1_score = ratings
+        .into_iter()
+        .filter(|rating| is_accepted(&rating, &workflows))
+        .map(|rating| rating.values().sum::<isize>())
+        .sum::<isize>();
+
+    let combinations = number_combinations(&workflows);
+
+    (part_1_score.to_string(), combinations.to_string())
+}
+
+fn number_combinations(workflows: &Workflows) -> isize {
+    let range: HashMap<char, (isize, isize)> = HashMap::from([
+        ('x', (1, 4000)),
+        ('m', (1, 4000)),
+        ('a', (1, 4000)),
+        ('s', (1, 4000)),
+    ]);
+
+    branch_combinations(&range, "in".to_string(), &workflows)
+}
+
+fn branch_combinations(
+    ranges: &HashMap<char, (isize, isize)>,
+    workflow_address: String,
+    workflows: &Workflows,
+) -> isize {
+    if &workflow_address == "R" {
+        return 0;
+    }
+
+    if &workflow_address == "A" {
+        return ranges.values().map(|(a, b)| (b - a) + 1).product::<isize>();
+    }
+
+    let workflow = workflows.get(&workflow_address).unwrap();
+    let mut combinations = 0;
+    let mut ranges = ranges.clone();
+
+    for rule in workflow {
+        let goto = rule.goto.as_str();
+
+        ranges = if let Some((category, operator, value)) = rule.condition.to_owned() {
+            // there's a condition, how much of that range is passed through vs not?
+
+            let category_range = *ranges.get(&category).unwrap();
+
+            if value < category_range.0 || value > category_range.1 {
+                // the condition isn't accepted at all, return the full range to continue processing
+                ranges
+            } else {
+                // the condition is accepted, parse the possibilities
+                let mut low_range = ranges.clone();
+                let mut high_range = ranges.clone();
+
+                let (satisfied, unsatisfied) = if operator == '<' {
+                    low_range.insert(category, (category_range.0, value - 1));
+                    high_range.insert(category, (value, category_range.1));
+
+                    (low_range, high_range)
+                } else if operator == '>' {
+                    low_range.insert(category, (category_range.0, value));
+                    high_range.insert(category, (value + 1, category_range.1));
+
+                    (high_range, low_range)
+                } else {
+                    panic!("Unexpected operator");
+                };
+
+                combinations += branch_combinations(&satisfied, goto.to_string(), &workflows);
+
+                unsatisfied
+            }
+        } else {
+            // no condition, it naively goes to the next node
+            combinations += branch_combinations(&ranges, goto.to_string(), &workflows);
+
+            break;
+        };
+    }
+
+    combinations
+}
+
+fn is_accepted(rating: &Rating, workflows: &Workflows) -> bool {
+    let mut workflow = workflows.get("in").unwrap();
+
+    loop {
+        let workflow_address = next_workflow(&rating, &workflow);
+
+        if workflow_address == "A" {
+            return true;
+        } else if workflow_address == "R" {
+            return false;
+        }
+
+        workflow = workflows.get(&workflow_address).unwrap();
+    }
+}
+
+fn next_workflow(rating: &Rating, workflow: &Vec<Rule>) -> String {
+    for rule in workflow {
+        if let Some(condition) = rule.condition.to_owned() {
+            if evaluation_condition(&rating, &condition) {
+                return rule.goto.to_owned();
+            }
+        } else {
+            return rule.goto.to_owned();
+        }
+    }
+
+    panic!("Ran out of rules!")
+}
+
+fn evaluation_condition(rating: &Rating, condition: &(char, char, isize)) -> bool {
+    let (category, operator, value) = condition;
+
+    let multiplier = match operator {
+        '>' => -1,
+        '<' => 1,
+        _ => panic!("Unexpected comparison"),
+    };
+
+    let rating_value = *rating.get(&category).unwrap() * multiplier;
+    let comparison_value = value * multiplier;
+
+    rating_value < comparison_value
 }
