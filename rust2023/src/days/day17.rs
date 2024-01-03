68872662aafedf6900ead82c93f5ee3de4a65c70
David Pugh <plastonick@gmail.com>
2024-01-03 09:15:02 +0000

    day 17... push up solution for part 2, refactored part 1

--- rust2023/src/days/day17.rs
+++ rust2023/src/days/day17.rs
@@ -4,50 +4,75 @@ use crate::Args;
 use pathfinding::prelude::dijkstra;
 use std::collections::HashMap;
 
-#[derive(Debug, Clone)]
-struct Particle {
-    position: Vector,
-    direction: Vector,
-    count: i8,
-    value: usize,
-    path: Vec<Vector>,
-}
-
 struct Map {
     tiles: HashMap<Vector, usize>,
     size: Vector,
 }
 
-static MAX_LINE: u8 = 3;
-
 impl Vector {
     fn successors(
         &self,
         map: &Map,
-        direction: &Vector,
-        count: &u8,
-    ) -> Vec<((Vector, Vector, u8), usize)> {
+        previous_direction: &Vector,
+        (min, max): &(isize, isize), // how many times we must move in the next direction
+    ) -> Vec<((Vector, Vector), usize)> {
         fn in_bounds(x: &Vector, map: &Map) -> bool {
             x.row >= 0 && x.col >= 0 && x.row <= map.size.row && x.col <= map.size.col
         }
 
-        let directions = vec![
-            Vector { row: 1, col: 0 },
-            Vector { row: -1, col: 0 },
-            Vector { row: 0, col: 1 },
-            Vector { row: 0, col: -1 },
-        ];
+        let directions = if previous_direction.row == previous_direction.col {
+            vec![Vector { row: 1, col: 0 }, Vector { row: 0, col: 1 }]
+        } else {
+            vec![
+                Vector {
+                    row: previous_direction.col,
+                    col: previous_direction.row,
+                },
+                Vector {
+                    row: -previous_direction.col,
+                    col: -previous_direction.row,
+                },
+            ]
+        };
 
-        directions
-            .into_iter()
-            .filter(|d| !d.eq(&direction.mul(-1)))
-            .filter(|d| !d.eq(&direction) || count < &MAX_LINE)
+        // possible delta paths to take from current position
+        // every combination of directions and numbers in (min..=max) range
+        let deltas = directions
+            .iter()
             .map(|d| {
-                let new_count = if d.eq(&direction) { count + 1 } else { 1 };
-                (self.add(&d), d, new_count)
+                (*min..=*max)
+                    .into_iter()
+                    .map(|steps| {
+                        (1..=steps)
+                            .into_iter()
+                            .map(|step| (d.mul(step), d.clone()))
+                            .collect::<Vec<(Vector, Vector)>>()
+                    })
+                    .collect::<Vec<Vec<(Vector, Vector)>>>()
             })
-            .filter(|(x, _, _)| in_bounds(&x, &map))
-            .map(|x| (x, *map.tiles.get(&x.0).unwrap()))
+            .flatten()
+            .collect::<Vec<Vec<(Vector, Vector)>>>();
+
+        deltas
+            .into_iter()
+            .map(|deltas| {
+                deltas
+                    .into_iter()
+                    .map(|(delta, dir)| (self.add(&delta), dir))
+                    .collect::<Vec<(Vector, Vector)>>()
+            })
+            .filter(|path| in_bounds(&path.last().unwrap().0, &map))
+            .map(|path| {
+                // dbg!(&path);
+
+                let cost = path
+                    .iter()
+                    .map(|(pos, _)| map.tiles.get(&pos).unwrap())
+                    .sum::<usize>();
+
+                // we care about where we end up, and the cost it takes to get there
+                (path.last().unwrap().clone(), cost)
+            })
             .collect()
     }
 }
@@ -90,23 +115,23 @@ pub fn run(args: &Args) -> Answer {
         tiles,
         size: Vector { row, col },
     };
+    let target = Vector { row, col };
 
-    let result = dijkstra(
-        &(Vector { row: 0, col: 0 }, Vector { row: 0, col: 0 }, 0_u8),
-        |(x, direction, count)| x.successors(&map, &direction, count),
-        |(x, _, _)| x.eq(&Vector { row, col }),
+    let result_part_1 = find_path(&map, &target, (1, 3));
+    let result_part_2 = find_path(&map, &target, (4, 10));
+
+    (result_part_1.1.to_string(), result_part_2.1.to_string())
+}
+
+fn find_path(
+    map: &Map,
+    target: &Vector,
+    stepping: (isize, isize),
+) -> (Vec<(Vector, Vector)>, usize) {
+    dijkstra(
+        &(Vector { row: 0, col: 0 }, Vector { row: 0, col: 0 }),
+        |(pos, d)| pos.successors(&map, &d, &stepping),
+        |(x, _)| x.eq(&target),
     )
-    .expect("No path found");
-
-    dbg!(&result);
-
-    _print_vec(
-        &result
-            .0
-            .iter()
-            .map(|x| x.0.clone())
-            .collect::<Vec<Vector>>(),
-    );
-
-    (result.1.to_string(), "".to_string())
+    .expect("No path found")
 }
