8d330998a58f059c66c005b61a44d65b9e3b9b31
David Pugh <plastonick@gmail.com>
2026-03-22 07:47:09 +0000

    working day 22 part 2

--- rust2023/src/days/day22.rs
+++ rust2023/src/days/day22.rs
@@ -1,6 +1,6 @@
 use crate::common::{get_lines, Answer};
-use crate::Args;
-use std::collections::VecDeque;
+use crate::{get_epoch_ms, Args};
+use std::collections::{HashSet, VecDeque};
 
 type Brick = (Vec<u32>, Vec<u32>);
 
@@ -37,6 +37,15 @@ pub fn run(_args: &Args) -> Answer {
         dropped_bricks.push(settled);
     }
 
+    let (depends_on, resting_on) = build_dependency_tree(&dropped_bricks);
+    let func_start = get_epoch_ms();
+
+    let part_2 = (0..dropped_bricks.len())
+        .map(|idx| (disintegration_value(idx, &depends_on, &resting_on).len() - 1) as u32)
+        .sum::<u32>();
+
+    let duration = get_epoch_ms() - func_start;
+    println!("part 2 {}s", duration);
     // assume they're safe to disintegrate
     let mut safe_to_disintegrate = vec![true; dropped_bricks.len()];
     for (_, rests_on) in dropped_bricks {
@@ -52,8 +61,49 @@ pub fn run(_args: &Args) -> Answer {
             .filter(|x| **x)
             .count()
             .to_string(),
-        "".to_string(),
+        part_2.to_string(),
     )
+}
+
+fn build_dependency_tree(
+    dropped_bricks: &Vec<(Brick, Vec<usize>)>,
+) -> (Vec<HashSet<usize>>, Vec<HashSet<usize>>) {
+    let mut depends_on = vec![HashSet::new(); dropped_bricks.len()];
+    let mut resting_on = Vec::with_capacity(dropped_bricks.len());
+
+    for (idx, (_, rests_on)) in dropped_bricks.iter().enumerate() {
+        resting_on.push(HashSet::from_iter(rests_on.clone().into_iter()));
+
+        for r_idx in rests_on {
+            depends_on[*r_idx].insert(idx);
+        }
+    }
+
+    (depends_on, resting_on)
+}
+
+fn disintegration_value(
+    brick_idx: usize,
+    depends_on: &Vec<HashSet<usize>>,
+    resting_on: &Vec<HashSet<usize>>,
+) -> HashSet<usize> {
+    let mut disintegrated_bricks = HashSet::from_iter([brick_idx]);
+
+    // push all the bricks that depend on our target brick into the wobblies
+    let mut wobbly = VecDeque::from_iter(&depends_on[brick_idx]);
+    while let Some(&idx) = wobbly.pop_front() {
+        if resting_on[idx].is_subset(&disintegrated_bricks) {
+            // this one bites the dust too!
+            disintegrated_bricks.insert(idx);
+
+            // and the ones that depend on it become wobbly!
+            for r_idx in &depends_on[idx] {
+                wobbly.push_back(r_idx);
+            }
+        }
+    }
+
+    disintegrated_bricks
 }
 
 fn drop_next_brick(brick: Brick, settled: &Vec<(Brick, Vec<usize>)>) -> (Brick, Vec<usize>) {
